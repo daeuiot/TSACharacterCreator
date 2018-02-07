1e68a78d985fd6cc8a7655171e0749e74d198a1c
daeuiot <daeuiot@gmail.com>
2018-02-08 05:04:55 +1100

    Ground work
    Added just a bit of code to have a little bit of a start

---
+++ TSACharacterCreator/src/daeuiot/tsacharactercreator/PlayerCharacter.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daeuiot.tsacharactercreator;
+
+/**
+ *
+ * @author Daeuiot
+ * 
+ * This class is used to hold all of the data for any given character
+ */
+public class PlayerCharacter {
+    
+}
--- TSACharacterCreator/src/daeuiot/tsacharactercreator/TSACharacterCreator.java
+++ TSACharacterCreator/src/daeuiot/tsacharactercreator/TSACharacterCreator.java
@@ -11,6 +11,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -21,24 +22,37 @@ public class TSACharacterCreator extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        build(primaryStage, 1200, 800);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        primaryStage.setTitle("Avatar: The Second Age - Character Creator");
         primaryStage.show();
+    }
+    /**
+     * Used to build the primary stage
+     * 
+     * @param primaryStage The stage with which to build
+     * @param windowWidth The width of the window
+     * @param windowHeight The height of the window
+     */
+    private void build(Stage primaryStage, int windowWidth, int windowHeight)
+    {       
+        VBox tabs = new VBox(); //Used to hold the tabs for the categories ie. Background/Class/Skills/etc.
+        tabs.setPrefWidth(100);
+        Button btnBackground    = new Button("Background");
+        Button btnAttributes    = new Button("Attributes");
+        Button btnSkills        = new Button("Skills");
+        tabs.getChildren().addAll(btnBackground, btnAttributes, btnSkills);
+        tabs.setStyle("-fx-spacing: 10; -fx-alignment: center; -fx-padding: 10; -fx-background-color: pink");
+        btnBackground.setMinWidth(tabs.getPrefWidth());
+        btnAttributes.setMinWidth(tabs.getPrefWidth());
+        btnSkills.setMinWidth(tabs.getPrefWidth());
+        tabs.setMaxSize(150, windowHeight);
+        
+        VBox root = new VBox(tabs);
+        
+        Scene scene = new Scene(root, windowWidth, windowHeight);
+        
+        primaryStage.setScene(scene);
     }
 
     /**
